ca6b302d114809b62721c4d3c19d8ed2ab96815d
huangzhuo <huangzhuo@inmindglobal.com>
2019-07-29 13:15:35 +0800

    update

--- regen.sh
+++ regen.sh
@@ -2,19 +2,6 @@
 
 hexo clean
 hexo g
-rm -rf public/uploads/*.jpg
-rm -rf public/uploads/git-cmd-summary
-rm -rf public/uploads/graphql
-rm -rf public/uploads/news
-rm -rf public/uploads/post-grpc
-rm -rf public/uploads/tesseract
-rm -rf public/uploads/uwsgi-gunicorn
-rm -rf public/uploads/mysql-groupby
-rm -rf public/images
-rm -rf public/css
-rm -rf public/2016*
-rm -rf public/2017*
-rm -rf public/2018*
 
 chmod -R 755 public/*
 
